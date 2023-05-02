67d08addcefd8719f204cf3fcae8b7d7e5c513d1
aaliaksandrau <a.aliaksandrau@elinext.com>
2023-05-02 23:22:02 +0400

    GQL-002 feat: add RTK

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'robohash.org', //TODO remove it it used for example images
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
